aa247421d3c8ac3d3232bdbd7eeab1bc0cfd456e
haiweizhao27 <2949088909@qq.com>
2026-09-19 14:39:19 +0800

    Add build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// 顶层构建脚本：只声明插件版本，不在此处应用插件。
+plugins {
+    id("com.android.application") version "8.7.3" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.devtools.ksp") version "1.9.24-1.0.20" apply false
+}
